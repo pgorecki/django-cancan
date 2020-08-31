4e24a0398b292a0ddadb3c13b2420cee7f5630c3
Przemysław Górecki <przemyslaw.gorecki@gmail.com>
2020-08-31 21:41:27 +0200

    travis

--- cancan/testapp/tests/test_core.py
+++ cancan/testapp/tests/test_core.py
@@ -3,74 +3,78 @@ from cancan.testapp.models import Article, User
 from cancan.ability import Ability, AbilityValidator
 
 
-class ModelAbilitiesTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(username="user1")
+class FooTestCase(TestCase):
+    def test_foo(self):
+        self.assertTrue(False)
 
-    def test_no_abilities_when_initialized(self):
-        ability = Ability(user=self.user)
-        validator = AbilityValidator(ability)
-        self.assertFalse(ability.can('view', Article))
+# class ModelAbilitiesTestCase(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create(username="user1")
 
-    def test_happy_path(self):
-        ability = Ability(user=self.user)
-        ability.can('view', Article)
-        validator = AbilityValidator(ability)
-        self.assertTrue(validator.can('view', Article))
+#     def test_no_abilities_when_initialized(self):
+#         ability = Ability(user=self.user)
+#         validator = AbilityValidator(ability)
+#         self.assertFalse(ability.can('view', Article))
 
-    def test_unknown_name(self):
-        ability = Ability(user=self.user)
-        ability.can('view', Article)
-        validator = AbilityValidator(ability)
-        self.assertFalse(validator.can('read', Article))
+#     def test_happy_path(self):
+#         ability = Ability(user=self.user)
+#         ability.can('view', Article)
+#         validator = AbilityValidator(ability)
+#         self.assertTrue(validator.can('view', Article))
 
-
-class ObjectAbilitiesTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(username='user1')
-        ability = Ability(user=self.user)
-        ability.can('view', Article, is_published=True)
-        self.validator = AbilityValidator(ability)
-
-    def test_can_view_published(self):
-        article = Article.objects.create(name='test', is_published=True)
-        self.assertTrue(self.validator.can(
-            'view', article))
-
-    def test_cannot_view_unpublished(self):
-        article = Article.objects.create(name='test', is_published=False)
-        self.assertFalse(self.validator.can(
-            'view', article))
-
-    def test_get_queryset1(self):
-        article1 = Article.objects.create(name='test', is_published=True)
-        qs = self.validator.queryset_for('view', Article)
-        self.assertEqual(qs.count(), 1)
-
-    def test_get_queryset2(self):
-        article1 = Article.objects.create(name='test', is_published=False)
-        qs = self.validator.queryset_for('view', Article)
-        self.assertEqual(qs.count(), 0)
+#     def test_unknown_name(self):
+#         ability = Ability(user=self.user)
+#         ability.can('view', Article)
+#         validator = AbilityValidator(ability)
+#         self.assertFalse(validator.can('read', Article))
 
 
-class ObjectAbilitiesMultipleCanTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(username='user1')
-        ability = Ability(user=self.user)
-        ability.can('view', Article, is_published=True)
-        ability.can('view', Article, created_by=self.user)
-        self.validator = AbilityValidator(ability)
+# class ObjectAbilitiesTestCase(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create(username='user1')
+#         ability = Ability(user=self.user)
+#         ability.can('view', Article, is_published=True)
+#         self.validator = AbilityValidator(ability)
 
-    def test_can_view_published(self):
-        article1 = Article.objects.create(name='test', is_published=True)
-        article2 = Article.objects.create(name='test', created_by=self.user)
-        self.assertTrue(self.validator.can('view', article1))
-        self.assertTrue(self.validator.can('view', article2))
+#     def test_can_view_published(self):
+#         article = Article.objects.create(name='test', is_published=True)
+#         self.assertTrue(self.validator.can(
+#             'view', article))
 
-    def test_queryset_contails_all_allowed(self):
-        article1 = Article.objects.create(name='test', is_published=True)
-        article2 = Article.objects.create(name='test', created_by=self.user)
-        article3 = Article.objects.create(name='test', is_published=False)
-        qs = self.validator.queryset_for('view', Article)
-        self.assertEqual(qs.count(), 2)
-        self.assertFalse(article3 in qs.all())
+#     def test_cannot_view_unpublished(self):
+#         article = Article.objects.create(name='test', is_published=False)
+#         self.assertFalse(self.validator.can(
+#             'view', article))
+
+#     def test_get_queryset1(self):
+#         article1 = Article.objects.create(name='test', is_published=True)
+#         qs = self.validator.queryset_for('view', Article)
+#         self.assertEqual(qs.count(), 1)
+
+#     def test_get_queryset2(self):
+#         article1 = Article.objects.create(name='test', is_published=False)
+#         qs = self.validator.queryset_for('view', Article)
+#         self.assertEqual(qs.count(), 0)
+
+
+# class ObjectAbilitiesMultipleCanTestCase(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create(username='user1')
+#         ability = Ability(user=self.user)
+#         ability.can('view', Article, is_published=True)
+#         ability.can('view', Article, created_by=self.user)
+#         self.validator = AbilityValidator(ability)
+
+#     def test_can_view_published(self):
+#         article1 = Article.objects.create(name='test', is_published=True)
+#         article2 = Article.objects.create(name='test', created_by=self.user)
+#         self.assertTrue(self.validator.can('view', article1))
+#         self.assertTrue(self.validator.can('view', article2))
+
+#     def test_queryset_contails_all_allowed(self):
+#         article1 = Article.objects.create(name='test', is_published=True)
+#         article2 = Article.objects.create(name='test', created_by=self.user)
+#         article3 = Article.objects.create(name='test', is_published=False)
+#         qs = self.validator.queryset_for('view', Article)
+#         self.assertEqual(qs.count(), 2)
+#         self.assertFalse(article3 in qs.all())
